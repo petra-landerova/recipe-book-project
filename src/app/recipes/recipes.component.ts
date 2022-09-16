fa5999405b78a2cd9555c4cab021ddaf0c524770
petra-landerova <petra.landerova@plus4u.net>
2022-09-16 18:06:24 +0200

    observables finish

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Recipe} from "./recipe.model";
-import {RecipeService} from "./recipe.service";
+
+import {RecipeService} from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -9,15 +9,10 @@ import {RecipeService} from "./recipe.service";
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
-  constructor(private recipeService: RecipeService) { }
+  constructor() {}
+
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    );
   }
 
 }
